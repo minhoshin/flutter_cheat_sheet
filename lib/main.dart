4ddbeefc517b143d5e198eabd031ad1ad9c57357
신민호 <shinminho@iyejin-ui-MacBookPro.local>
2021-03-15 01:58:23 +0900

    http

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -22,6 +23,13 @@ class HttpApp extends StatefulWidget {
 
 class _HttpAppState extends State<HttpApp> {
   String result = '';
+  List data;
+
+  @override
+  void initState() {
+    super.initState();
+    data = new List();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +43,30 @@ class _HttpAppState extends State<HttpApp> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          // var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+          // var response = await http.get(url);
+          // setState(() {
+          //   result = response.body;
+          // });
+          getJSONData();
+
+        },
         child: Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url =
+        'https://dapi.kakao.com/v3/search/book?target=title&query=주';
+    var response = await http.get(url, headers: {"Authorization": "KakaoAK a03be22b7e763784bf6f9c92f0c18201"});
+    print(response.body);
+    setState(() {
+      var dataConvertToJSJON = json.decode(response.body);
+      List result = dataConvertToJSJON['documents'];
+      data.addAll(result);
+    });
+    return response.body;
   }
 }
