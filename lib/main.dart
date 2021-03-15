8c2c47adf3cc7291321316c30b032b289d15589c
신민호 <shinminho@iyejin-ui-MacBookPro.local>
2021-03-16 01:36:34 +0900

    http

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'tabbar example';
+  static const String _title = 'Http Example';
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +22,72 @@ class HttpApp extends StatefulWidget {
 }
 
 class _HttpAppState extends State<HttpApp> {
-  String result = '';
+  // String result = '';
   List data;
+  TextEditingController _editingController;
 
   @override
   void initState() {
     super.initState();
     data = new List();
+    _editingController = new TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Example'),
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요'),
+        ),
       ),
       body: Container(
         child: Center(
-          child: Text('$result'),
+          // child: Text('$data'),
+          child: data.length == 0
+              ? Text(
+                  '데이터가 없습니다.',
+                  style: TextStyle(fontSize: 30.0),
+                  textAlign: TextAlign.center,
+                )
+              // : Text('데이터가 있습니다.'),
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Image.network(
+                              data[index]['thumbnail'],
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.contain,
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width - 150,
+                                  child: Text(
+                                    data[index]['title'].toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                Text(data[index]['authors'].toString()),
+                                Text(data[index]['sale_price'].toString()),
+                                Text(data[index]['status'].toString()),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: data.length,
+                ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -50,7 +98,6 @@ class _HttpAppState extends State<HttpApp> {
           //   result = response.body;
           // });
           getJSONData();
-
         },
         child: Icon(Icons.file_download),
       ),
@@ -58,13 +105,15 @@ class _HttpAppState extends State<HttpApp> {
   }
 
   Future<String> getJSONData() async {
-    var url =
-        'https://dapi.kakao.com/v3/search/book?target=title&query=주';
-    var response = await http.get(url, headers: {"Authorization": "KakaoAK a03be22b7e763784bf6f9c92f0c18201"});
+    print('$_editingController');
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=${_editingController.value.text}';
+    var response = await http.get(url,
+        headers: {"Authorization": "KakaoAK a03be22b7e763784bf6f9c92f0c18201"});
     print(response.body);
     setState(() {
       var dataConvertToJSJON = json.decode(response.body);
       List result = dataConvertToJSJON['documents'];
+      data.clear();
       data.addAll(result);
     });
     return response.body;
